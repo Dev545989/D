bf3403247875c5ce5238ca22990caa7e8ae79552
wwo115570-commits <wwo115570@gmail.com>
2026-09-07 10:02:15 +0300

    fix secrets

--- r2_property_enrichment.py
+++ r2_property_enrichment.py
@@ -44,10 +44,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-R2_BUCKET = os.environ["CF_R2_BUCKET_NAME"]
-R2_ENDPOINT = os.environ["CF_R2_ENDPOINT_URL"]
-R2_ACCESS_KEY = os.environ["CF_R2_ACCESS_KEY_ID"]
-R2_SECRET_KEY = os.environ["CF_R2_SECRET_ACCESS_KEY"]
+R2_ACCESS_KEY = os.getenv("CF_R2_ACCESS_KEY_ID")
+R2_SECRET_KEY = os.getenv("CF_R2_SECRET_ACCESS_KEY")
+R2_ENDPOINT = os.getenv("CF_R2_ENDPOINT_URL", "").rstrip("/")
+R2_BUCKET = os.getenv("CF_R2_BUCKET_NAME", "")
 
 # NOTE: the property scraper (rent_property.yml -> final_merge) saves under
 # f"DUAE/{date_prefix}/{category_path}/..." where date_prefix is built from
